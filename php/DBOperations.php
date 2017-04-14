b774b9fe2929fed615d459ace17ffe0af00783a7
Aniket Katkar <locked.ani@gmail.com>
2017-04-14 20:14:34 +0530

    added php files for host server

---
+++ php/DBOperations.php
@@ -0,0 +1,217 @@
+<?php
+ 
+class DBOperations{
+ 
+    private $host = '<hostname>';
+    private $user = '<database user name>';
+    private $db = '<database name>';
+    private $pass = '<database password>';
+    private $conn;
+ 
+public function __construct() {
+ 
+   $this -> conn = new PDO("mysql:host=".$this -> host.";dbname=".$this -> db, $this -> user, $this -> pass);
+ 
+}
+ 
+ public function insertData($name,$email,$password){
+ 
+   $unique_id = uniqid('', true);
+    $hash = $this->getHash($password);
+    $encrypted_password = $hash["encrypted"];
+   $salt = $hash["salt"];
+ 
+   $sql = 'INSERT INTO users SET unique_id =:unique_id,name =:name,
+    email =:email,encrypted_password =:encrypted_password,salt =:salt,created_at = NOW()';
+ 
+   $query = $this ->conn ->prepare($sql);
+   $query->execute(array('unique_id' => $unique_id, ':name' => $name, ':email' => $email,
+     ':encrypted_password' => $encrypted_password, ':salt' => $salt));
+ 
+    if ($query) {
+ 
+        return true;
+ 
+    } else {
+ 
+        return false;
+ 
+    }
+ }
+ 
+ public function checkLogin($email, $password) {
+ 
+    $sql = 'SELECT * FROM users WHERE email = :email';
+    $query = $this -> conn -> prepare($sql);
+    $query -> execute(array(':email' => $email));
+    $data = $query -> fetchObject();
+    $salt = $data -> salt;
+    $db_encrypted_password = $data -> encrypted_password;
+ 
+    if ($this -> verifyHash($password.$salt,$db_encrypted_password) ) {
+ 
+        $user["name"] = $data -> name;
+        $user["email"] = $data -> email;
+        $user["unique_id"] = $data -> unique_id;
+        return $user;
+ 
+    } else {
+ 
+        return false;
+    }
+ }
+ 
+ public function changePassword($email, $password){
+ 
+    $hash = $this -> getHash($password);
+    $encrypted_password = $hash["encrypted"];
+    $salt = $hash["salt"];
+ 
+    $sql = 'UPDATE users SET encrypted_password = :encrypted_password, salt = :salt WHERE email = :email';
+    $query = $this -> conn -> prepare($sql);
+    $query -> execute(array(':email' => $email, ':encrypted_password' => $encrypted_password, ':salt' => $salt));
+ 
+    if ($query) {
+ 
+        return true;
+ 
+    } else {
+ 
+        return false;
+ 
+    }
+ }
+ 
+ public function checkUserExist($email){
+ 
+    $sql = 'SELECT COUNT(*) from users WHERE email =:email';
+    $query = $this -> conn -> prepare($sql);
+    $query -> execute(array('email' => $email));
+ 
+    if($query){
+ 
+        $row_count = $query -> fetchColumn();
+ 
+        if ($row_count == 0){
+ 
+            return false;
+ 
+        } else {
+ 
+            return true;
+ 
+        }
+    } else {
+ 
+        return false;
+    }
+ }
+ 
+ public function getHash($password) {
+ 
+     $salt = sha1(rand());
+     $salt = substr($salt, 0, 10);
+     $encrypted = password_hash($password.$salt, PASSWORD_DEFAULT);
+     $hash = array("salt" => $salt, "encrypted" => $encrypted);
+ 
+     return $hash;
+ 
+}
+ 
+public function verifyHash($password, $hash) {
+ 
+    return password_verify ($password, $hash);
+}
+    
+//new code
+public function passwordResetRequest($email){
+ 
+        $random_string = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 6)), 0, 6);
+        $hash = $this->getHash($random_string);
+        $encrypted_temp_password = $hash["encrypted"];
+        $salt = $hash["salt"];
+ 
+        $sql = 'SELECT COUNT(*) from password_reset_request WHERE email =:email';
+        $query = $this -> conn -> prepare($sql);
+        $query -> execute(array('email' => $email));
+ 
+        if($query){
+ 
+            $row_count = $query -> fetchColumn();
+ 
+            if ($row_count == 0){
+ 
+                $insert_sql = 'INSERT INTO password_reset_request SET email =:email,encrypted_temp_password =:encrypted_temp_password,
+                salt =:salt,created_at = :created_at';
+                $insert_query = $this ->conn ->prepare($insert_sql);
+                $insert_query->execute(array(':email' => $email, ':encrypted_temp_password' => $encrypted_temp_password,
+                        ':salt' => $salt, ':created_at' => date("Y-m-d H:i:s")));
+ 
+                if ($insert_query) {
+ 
+                    $user["email"] = $email;
+                    $user["temp_password"] = $random_string;
+ 
+                    return $user;
+ 
+                } else {
+ 
+                    return false;
+ 
+                }
+            } else {
+ 
+                $update_sql = 'UPDATE password_reset_request SET email =:email,encrypted_temp_password =:encrypted_temp_password,
+                salt =:salt,created_at = :created_at';
+                $update_query = $this -> conn -> prepare($update_sql);
+                $update_query -> execute(array(':email' => $email, ':encrypted_temp_password' => $encrypted_temp_password,
+                        ':salt' => $salt, ':created_at' => date("Y-m-d H:i:s")));
+ 
+                if ($update_query) {
+ 
+                    $user["email"] = $email;
+                    $user["temp_password"] = $random_string;
+                    return $user;
+ 
+                } else {
+ 
+                    return false;
+ 
+                }
+            }
+        } else {
+ 
+            return false;
+        }
+    }
+ 
+    public function resetPassword($email,$code,$password){
+ 
+        $sql = 'SELECT * FROM password_reset_request WHERE email = :email';
+        $query = $this -> conn -> prepare($sql);
+        $query -> execute(array(':email' => $email));
+        $data = $query -> fetchObject();
+        $salt = $data -> salt;
+        $db_encrypted_temp_password = $data -> encrypted_temp_password;
+ 
+        if ($this -> verifyHash($code.$salt,$db_encrypted_temp_password) ) {
+ 
+            $old = new DateTime($data -> created_at);
+            $now = new DateTime(date("Y-m-d H:i:s"));
+            $diff = $now->getTimestamp() - $old->getTimestamp();
+ 
+            if($diff < 120) {
+ 
+                return $this -> changePassword($email, $password);
+ 
+            } else {
+ 
+                false;
+            }
+        } else {
+ 
+            return false;
+        }
+    }
+    
+}
---
+++ php/Functions.php
@@ -0,0 +1,275 @@
+<?php
+ 
+require_once 'DBOperations.php';
+require 'PHPMailerAutoload.php';
+
+ 
+class Functions{
+ 
+private $db;
+private $mail;
+ 
+public function __construct() {
+ 
+      $this -> db = new DBOperations();
+      $this -> mail = new PHPMailer;
+ 
+}
+ 
+public function registerUser($name, $email, $password) {
+ 
+   $db = $this -> db;
+ 
+   if (!empty($name) && !empty($email) && !empty($password)) {
+ 
+      if ($db -> checkUserExist($email)) {
+ 
+         $response["result"] = "failure";
+         $response["message"] = "User Already Registered !";
+         return json_encode($response);
+ 
+      } else {
+ 
+         $result = $db -> insertData($name, $email, $password);
+ 
+         if ($result) {
+ 
+              $response["result"] = "success";
+            $response["message"] = "User Registered Successfully !";
+            return json_encode($response);
+ 
+         } else {
+ 
+            $response["result"] = "failure";
+            $response["message"] = "Registration Failure";
+            return json_encode($response);
+ 
+         }
+      }
+   } else {
+ 
+      return $this -> getMsgParamNotEmpty();
+ 
+   }
+}
+ 
+public function loginUser($email, $password) {
+ 
+  $db = $this -> db;
+ 
+  if (!empty($email) && !empty($password)) {
+ 
+    if ($db -> checkUserExist($email)) {
+ 
+       $result =  $db -> checkLogin($email, $password);
+ 
+       if(!$result) {
+ 
+        $response["result"] = "failure";
+        $response["message"] = "Invaild Login Credentials";
+        return json_encode($response);
+ 
+       } else {
+ 
+        $response["result"] = "success";
+        $response["message"] = "Login Sucessful";
+        $response["user"] = $result;
+        return json_encode($response);
+ 
+       }
+    } else {
+ 
+      $response["result"] = "failure";
+      $response["message"] = "Invaild Login Credentials";
+      return json_encode($response);
+ 
+    }
+  } else {
+ 
+      return $this -> getMsgParamNotEmpty();
+    }
+}
+ 
+public function changePassword($email, $old_password, $new_password) {
+ 
+  $db = $this -> db;
+ 
+  if (!empty($email) && !empty($old_password) && !empty($new_password)) {
+ 
+    if(!$db -> checkLogin($email, $old_password)){
+ 
+      $response["result"] = "failure";
+      $response["message"] = 'Invalid Old Password';
+      return json_encode($response);
+ 
+    } else {
+ 
+    $result = $db -> changePassword($email, $new_password);
+ 
+      if($result) {
+ 
+        $response["result"] = "success";
+        $response["message"] = "Password Changed Successfully";
+        return json_encode($response);
+ 
+      } else {
+ 
+        $response["result"] = "failure";
+        $response["message"] = 'Error Updating Password';
+        return json_encode($response);
+ 
+      }
+    }
+  } else {
+ 
+      return $this -> getMsgParamNotEmpty();
+  }
+}
+ 
+public function isEmailValid($email){
+ 
+  return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+ 
+public function getMsgParamNotEmpty(){
+ 
+  $response["result"] = "failure";
+  $response["message"] = "Parameters should not be empty !";
+  return json_encode($response);
+ 
+}
+ 
+public function getMsgInvalidParam(){
+ 
+  $response["result"] = "failure";
+  $response["message"] = "Invalid Parameters";
+  return json_encode($response);
+ 
+}
+ 
+public function getMsgInvalidEmail(){
+ 
+  $response["result"] = "failure";
+  $response["message"] = "Invalid Email";
+  return json_encode($response);
+ 
+}
+    
+    
+//new code
+public function resetPasswordRequest($email){
+ 
+        $db = $this -> db;
+ 
+        if ($db -> checkUserExist($email)) {
+ 
+            $result =  $db -> passwordResetRequest($email);
+ 
+            if(!$result){
+ 
+                $response["result"] = "failure";
+                $response["message"] = "Reset Password Failure";
+                return json_encode($response);
+ 
+            } else {
+ 
+                $mail_result = $this -> sendPHPMail($result["email"],$result["temp_password"]);
+ 
+                if($mail_result){
+ 
+                    $response["result"] = "success";
+                    $response["message"] = "Check your mail for reset password code.";
+                    return json_encode($response);
+ 
+                } else {
+ 
+                    $response["result"] = "failure";
+                    $response["message"] = "Reset Password Failure";
+                    return json_encode($response);
+                }
+            }
+        } else {
+ 
+            $response["result"] = "failure";
+            $response["message"] = "Email does not exist";
+            return json_encode($response);
+ 
+        }
+    }
+ 
+    public function resetPassword($email,$code,$password){
+ 
+        $db = $this -> db;
+ 
+        if ($db -> checkUserExist($email)) {
+ 
+            $result =  $db -> resetPassword($email,$code,$password);
+ 
+            if(!$result){
+ 
+                $response["result"] = "failure";
+                $response["message"] = "Reset Password Failure";
+                return json_encode($response);
+ 
+            } else {
+ 
+                $response["result"] = "success";
+                $response["message"] = "Password Changed Successfully";
+                return json_encode($response);
+ 
+            }
+        } else {
+ 
+            $response["result"] = "failure";
+            $response["message"] = "Email does not exist";
+            return json_encode($response);
+ 
+        }
+    }
+ 
+    /*public function sendEmail($email,$temp_password){
+ 
+        $mail = $this -> mail;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'your.email@gmail.com';
+        $mail->Password = 'password';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+ 
+        $mail->From = 'your.email@gmail.com';
+        $mail->FromName = 'Your Name';
+        $mail->addAddress($email, 'Your Name');
+ 
+        $mail->addReplyTo('your.email@gmail.com', 'Your Name');
+ 
+        $mail->WordWrap = 50;
+        $mail->isHTML(true);
+ 
+        $mail->Subject = 'Password Reset Request';
+        $mail->Body    = 'Hi,<br><br> Your password reset code is <b>'.$temp_password.'</b> . This code expires in 120 seconds. Enter this code within 120 seconds to reset your password.<br><br>Thanks,<br>Learn2Crack.';
+ 
+        if(!$mail->send()) {
+ 
+            return $mail->ErrorInfo;
+ 
+        } else {
+ 
+            return true;
+ 
+        }
+    }*/
+ 
+    public function sendPHPMail($email,$temp_password){
+ 
+        $subject = 'Password Reset Request';
+        $message = 'Hi, Your password reset code is '.$temp_password.' . This code expires in 5 minutes. Enter this code within 5 minutes to reset your password. ';
+        $from = "<email through which you want the mail to be sent";
+        $headers = "From: <email through which you want people to receive email from" . $from;
+ 
+        return mail($email,$subject,$message,$headers);
+ 
+    }
+    
+}
